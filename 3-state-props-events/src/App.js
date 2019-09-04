555dd725c98f45717067234dc8b7a75e47cae7fd
Madhv <madhv@example.com>
2019-09-04 23:35:09 +0530

    Props-state-event implemented

--- 3-state-props-events/src/App.js
+++ 3-state-props-events/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comp1 from './Comp1'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h3>General Demo of State and Props</h3>
+      
+       {/* Pass the Properties which you want inside the props */}
+      <Comp1 name='Diksha'/>
+     </div>
   );
 }
 
---
+++ 3-state-props-events/src/Comp1.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Comp1 extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        
+        this.state={name:props.name} // initialize the state from the props
+
+        // you also initilize like the below  {Destructuring}
+        // this.state=props;
+    }
+
+    // handle change event on textbox
+    //as state will change the render would be fire.
+    handleOnChange=(event)=>{this.setState({name:event.target.value})}
+
+    render(){
+        return(
+            <>
+            
+
+            <label>{this.state.name}</label> <br/>
+
+            <input type='text' placeholder='Enter your name' onChange={this.handleOnChange} />
+            </>
+        )
+    }
+}
+
+export default Comp1;
