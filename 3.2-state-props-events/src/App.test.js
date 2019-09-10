c225f0a79c776cd1b46ed9b44134453dd3df0e5c
madhvbucket <kushwanshb.tech@gmail.com>
2019-09-10 20:05:57 +0530

    Events

--- 3.2-state-props-events/src/App.test.js
+++ 3.2-state-props-events/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MainComp from './MainComp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MainComp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ 3.2-state-props-events/src/MainComp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import  ChildComp1 from './ChildComp1';
+import  ChildComp2 from './ChildComp2';
+
+const divStyle = {
+  paddding: '50px',
+  border: '5px solid black'
+};
+
+const alignCenter = {
+  textAlign:'center'
+};
+// This is main Component 
+class MainComp extends React.Component  {
+
+  // It will called when the Childcom-1 Textbox would be change
+  handleOnChange=(event)=>{this.setState({myText:event.target.value})}
+
+  state={myText:'Initial Text'}
+
+  render(){
+  return (
+    <div style={divStyle}>
+      <label style={alignCenter}>This is Main component</label>
+     <ChildComp1 handleOnChange={this.handleOnChange}/> <br/>
+     <ChildComp2 myText={this.state.myText}/>
+    </div>
+  );
+  }
+}
+
+export default MainComp;
