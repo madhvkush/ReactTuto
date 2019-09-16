ff595c0e2867ed3db741cc039895686437f987a5
madhvbucket <kushwanshb.tech@gmail.com>
2019-09-16 16:13:53 +0530

    Hooks Tuto done

--- 4-hooks/src/App.js
+++ 4-hooks/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CompUSestate from './CompUSestate';
+import CompUseEffect from './CompUseEffect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hooks are used with functional components</h1>
+    <a target="_blank" href="https://reactjs.org/docs/hooks-overview.html">Reference</a>
+    <CompUSestate />
+    <CompUseEffect/>
     </div>
   );
 }
