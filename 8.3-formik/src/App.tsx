e3395473144e5dfe6d0917801b92a36954ec51bb
Madhv <madhv.net@gmail.com>
2022-12-07 09:55:03 +0530

    React Formik - use of formik.touched Property.

---
+++ 8.3-formik/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MyForm } from "./MyForm";
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
